946ac43311814d5073f1d80f66e0c1e4024ec3b0
sophiadt <sophiadnth@gmail.com>
2023-11-26 21:11:41 -0800

    added in template files

---
+++ main.js
@@ -0,0 +1,5 @@
+function red() {
+    console.log("Changing colour");
+    document.getElementById("button").style.color = "blue";
+    document.getElementById("button").style.backgroundColor = "pink";
+}
